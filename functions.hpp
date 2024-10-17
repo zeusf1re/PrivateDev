35bdec9d0e95bf1017ed19b9e2bc8bce0e638f9f
zeusf1re <akski@yahoo.com>
2024-10-17 18:38:02 +0300

    ErrorsTest rdy

--- functions.hpp
+++ functions.hpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <limits>
 #include <typeinfo>
-
+#include <string>
 struct ColorChoice {
  public:
     enum Colors {
@@ -20,51 +20,115 @@ struct ColorChoice {
 
 
 
-struct ErrorsTest {
-    public:
-        enum DataTypes {
-            Int,
-            Char,
-            Double
-        };
-        static void ErrorOutput() {
-            ColorChoice::SetColor(ColorChoice::red);
-            std::cout << "Неправильный формат ввода данных\n";
-            ColorChoice::ResetColor();
-            exit(1);
-        };
+// struct ErrorsTest {
+//     public:
+//         enum DataTypes {
+//             Int,
+//             Char,
+//             Double
+//         };
+//         static void ErrorOutput() {
+//             ColorChoice::SetColor(ColorChoice::red);
+//             std::cout << "Неправильный формат ввода данных\n";
+//             ColorChoice::ResetColor();
+//             exit(1);
+//         };
 
-        template <typename AnyType>
-        static AnyType CorrectInput(ErrorsTest::DataTypes wantedType) {
-            int Input = std::numeric_limits<int>::min();
-            const int minimum = std::numeric_limits<int>::min();
-            std::cout << Input;
-            switch (wantedType) {
-                case DataTypes::Int:
-                    {
-                        const int a{};
-                        std::cout << Input;
-                        std::cin >> Input;
-                        std::cout << Input;
-                        if (std::cin.peek() != '\n' || Input == minimum) {
-                            std::cin.clear();
-                            while(std::cin.get() != '\n') {};
-                            ErrorOutput();
-                            return 0;
-                        } else {
-                            return Input;
-                        }
-                        break;
-                    }
-                case DataTypes::Char:
-                    return 0;
-                    break;
-                case DataTypes::Double:
-                    return 0;
-                    break;
-                default:
-                    ErrorOutput();
-                    return 1;
+//         template <typename AnyType>
+//         static AnyType CorrectInput(ErrorsTest::DataTypes wantedType) {
+//             int Input = std::numeric_limits<int>::min();
+//             switch (wantedType) {
+//                 case DataTypes::Int:
+//                     {
+//                         const int startDefInput = std::numeric_limits<int>::min();
+//                         std::cin >> Input;
+//                         if (std::cin.get() != '\n' || Input == startDefInput) {
+//                             std::cin.clear();
+//                             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+//                             ErrorOutput();
+
+//                         } else {
+//                             return Input;
+//                         }
+//                     }
+//                 case DataTypes::Char:
+//                     return 0;
+//                     break;
+//                 case DataTypes::Double:
+//                     return 0;
+//                     break;
+//                 default:
+//                     ErrorOutput();
+//                     return 1;
+//             }
+//         };
+// };
+
+// #include <iostream>
+// #include <limits>
+// #include <string>
+// #include <sstream>
+
+// struct ColorChoice {
+//     static void SetColor(int color) {
+//         // Реализация изменения цвета текста
+//     }
+//     static void ResetColor() {
+//         // Реализация сброса цвета текста
+//     }
+// };
+
+int test(int&);
+
+
+struct ErrorsTest {
+public:
+    enum DataTypes {
+        Int,
+        Char,
+        Double
+    };
+
+    static void ErrorOutput() {
+        ColorChoice::SetColor(ColorChoice::Colors::red);
+        std::cout << "Неправильный формат ввода данных\n";
+        ColorChoice::ResetColor();
+        exit(1);
+    }
+    template<typename UserType>
+    static UserType CorrectInput(DataTypes wantedType) {
+        if(wantedType == DataTypes::Char) {
+            char firstSymbol = std::cin.get();
+            char secondSymbol = std::cin.get();
+            if (isalpha(firstSymbol) && secondSymbol == '\n') {
+                return firstSymbol;
             }
-        };
+            while(std::cin.get() != '\n');
+            ErrorOutput();
+        }
+
+        char firstSymbol = std::cin.peek();
+        if(!isdigit(firstSymbol)){
+            ErrorOutput();
+        }
+
+        double Input{};
+        std::cin >> Input;
+        std::cin.clear();
+
+        if(std::cin.get() != '\n') {
+            while(std::cin.get() != '\n');
+            ErrorOutput();
+        }
+
+        if (wantedType == DataTypes::Double){
+            return Input;
+        }
+        return static_cast<UserType>(Input);
+    }
+};
+
+struct a{
+    int a{};
+    int b{};
 };
--- main.cpp
+++ main.cpp
@@ -4,16 +4,34 @@
 
 #include "functions.hpp"
 
+void print(int a, int b){
+    std::cout << a << ' ' << b << '\n';
+}
+
+
+void testOutput(int a, int b){
+    std::cout << a << ' ' << b << '\n';
+}
+int test(int& prn){
+    int b{};
+    for(int i = 0; i < 10; i++){
+        b += i;
+        ++prn;
+    }
+    std::cout << b << '\n';
+    return b;
+}
 
 int main(int argc, char** argv) {
-    //int b = ErrorsTest::CorrectInput<int>(ErrorsTest::DataTypes::Int);
-    //std::cout << b << '\n';
-    int a = 1488;
-    std::cin >> a;
-    auto b = std::cin.get();
-    while (b != '\n') {
-        std::cout << b;
-        b = std::cin.get();
-    }
-    std::cout <<  a;
+    // a ex;
+    // ex.a = 123;
+    // ex.b = 456;
+    // print(ex.a, ex.b);
+    // int n = 1;
+    // int& prn = n;
+    // testOutput(n - 1, test(prn));
+    auto intTest = ErrorsTest::CorrectInput<int>(ErrorsTest::DataTypes::Int);
+    auto charTest = ErrorsTest::CorrectInput<char>(ErrorsTest::DataTypes::Char);
+    auto doubleTest = ErrorsTest::CorrectInput<double>(ErrorsTest::DataTypes::Double);
+    std::cout << intTest << ' ' << charTest << ' ' << sdoubleTest << '\n';
 }
