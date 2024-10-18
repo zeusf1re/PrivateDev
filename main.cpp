811f0ba4797be4dc75017931fd8b0efb6e31d6ca
zeusf1re <akski@yahoo.com>
2024-10-18 16:05:04 +0300

    sfk

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+#include "CorrectInput.hpp"
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv){
     std::cout << "Hello, world!" << std::endl;
-
+    int a{};
+    std::cin >> a;
+    std::cout << a << std::endl;
+    char firstLetter = std::cin.get();
+    std::cout << firstLetter << std::endl;
     return 0;
+    auto b = ErrorsTest::CorrectInput<char>(ErrorsTest::DataTypes::Char);
 }
